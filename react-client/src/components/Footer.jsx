0d1f9dea878335ab3ebd3a947ad5b1cd58ebf08a
Andrew Guise <aguise@umich.edu>
2019-07-01 20:09:59 -0400

    Add starter files

---
+++ react-client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+mport React, { Component } from 'react';
+class Footer extends Component {
+	  render() {
+		      return (
+			            <footer>
+			              <div>
+			                Footer component
+			              </div>
+			            </footer>
+			          )
+		    }
+}
+export default Footer;
